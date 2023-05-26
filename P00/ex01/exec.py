ca84473868ece045515ec0f7c532bffe427912c2
Astrid GAULTIER <asgaulti@e1r9p14.clusters.42paris.fr>
2023-05-26 17:27:09 +0200

    python ex01 - 1 : notes

---
+++ P00/ex01/exec.py
@@ -0,0 +1,8 @@
+1 initialiser variable msg = a l'argument 1 (comment le recup?)
+2 for (int i = 0; i < len(msg); i++)
+    if (msg[i] est upper)
+        msg[i].lower()
+    else
+        msg[i].upper()
+3 print le msg avec for (int i = len(msg); i > 0; i--)
+
