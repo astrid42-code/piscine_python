cde4b2aa52323a8db93898b750d3fd0121998687
Astrid GAULTIER <asgaulti@e1r9p15.clusters.42paris.fr>
2023-05-29 17:13:33 +0200

    python ex01 ok

--- P00/ex01/exec.py
+++ P00/ex01/exec.py
@@ -1,8 +1,14 @@
-1 initialiser variable msg = a l'argument 1 (comment le recup?)
-2 for (int i = 0; i < len(msg); i++)
-    if (msg[i] est upper)
-        msg[i].lower()
-    else
-        msg[i].upper()
-3 print le msg avec for (int i = len(msg); i > 0; i--)
+import sys
+# delete the first argv (file name)
+sys.argv.pop(0)
 
+str = ""
+for args in sys.argv[:]:
+    for char in args:
+        if (char.isupper()):
+            str += char.lower()
+        else:
+            str += char.upper()
+print(str[::-1])
+
+# Slice notation takes the form [start:stop:step]. In this case, we omit the start and stop positions since we want the whole string. We also use step = -1, which means, "repeatedly step from right to left by 1 character".
