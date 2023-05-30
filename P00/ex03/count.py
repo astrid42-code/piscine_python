382c3bc349041e2efc5e0ce0fda2264ad28c4d4e
Astrid GAULTIER <asgaulti@e1r2p10.clusters.42paris.fr>
2023-05-30 16:56:42 +0200

    python ex03 - notes

---
+++ P00/ex03/count.py
@@ -0,0 +1,18 @@
+import sys
+
+# Create a function called text_analyzer that takes a single string argument and displays
+# the sums of its upper-case characters, lower-case characters, punctuation characters and
+# spaces.
+# • If None or nothing is provided, the user is prompted to provide a string.
+# • If the argument is not a string, print an error message.
+# • This function must have a docstring explaning its behavior.
+# Test your function with the python console
+
+# Docstrings act as documentation for the class, module, and packages.
+# On the other hand, Comments are mainly used to explain non-obvious portions
+#  of the code and can be useful for comments on Fixing bugs and tasks 
+#  that are needed to be done.
+
+1 creer une fonction text_analyser(string str)
+2 sum(str.isupper() + str.islower() + str.is?? + str.isspace())
+3 cas d erreurs + sortie
