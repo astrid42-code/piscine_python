1be42a1b03a2376af9187c1952f42ed78b5e467d
Astrid GAULTIER <asgaulti@e3r5p8.clusters.42paris.fr>
2023-05-30 11:07:38 +0200

    python ex02 - looks ok

--- P00/ex02/whois.py
+++ P00/ex02/whois.py
@@ -1,13 +1,25 @@
 import sys
 
+if (len(sys.argv) == 1):
+	print("There is no argument")
+	sys.exit(1)
+if (len(sys.argv) > 2):
+	print("There are too many arguments")
+	sys.exit(1)
+
+# for args in sys.argv:
+    # for char in args:
 sys.argv.pop(0)
-
-i = 0
-
-for args in sys.argv[i:]:
-    for char in args:
-        nb = sys.argv[i]
-        if (nb.isdigit() == False):
-            print("AssertionError: argument is not an integer")
-    print(nb)
+# print(sys.argv)
+nb = sys.argv[0]
+if (nb.isdigit() == False):
+	print("AssertionError: argument is not an integer")
+	sys.exit(1)
+if int(nb) == 0:
+	print("I'm Zero")
+elif (int(nb) % 2) == 0:
+	print("I'm Even")
+else:
+	print("I'm Odd")
+sys.exit(1)
     
