0273023e19f5e4912077246e1d3d612163dbc54c
Astrid GAULTIER <asgaulti@bess-f1r1s9.clusters.42paris.fr>
2023-06-15 15:42:53 +0200

    python ex02 avec gestion nbrs neg

--- P00/ex02/whois.py
+++ P00/ex02/whois.py
@@ -4,22 +4,24 @@ if (len(sys.argv) == 1):
 	print("There is no argument")
 	sys.exit(1)
 if (len(sys.argv) > 2):
-	print("There are too many arguments")
+	# assert False, "There are too many arguments"
+	print("Assertion error: there are too many arguments")
 	sys.exit(1)
 
-# for args in sys.argv:
-    # for char in args:
 sys.argv.pop(0)
-# print(sys.argv)
-nb = sys.argv[0]
-if (nb.isdigit() == False):
-	print("AssertionError: argument is not an integer")
+
+try:
+	nb = int(sys.argv[0])
+except Exception:
+	# assert False, "argument is not an integer"
+	print("Assertion error: argument is not an integer")
 	sys.exit(1)
-if int(nb) == 0:
+
+if nb == 0:
 	print("I'm Zero")
-elif (int(nb) % 2) == 0:
+elif nb % 2 == 0:
 	print("I'm Even")
 else:
 	print("I'm Odd")
+
 sys.exit(1)
-    
