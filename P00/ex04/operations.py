ed73f1df72436635cda2606a51703df843919a44
Astrid GAULTIER <asgaulti@bess-f1r2s9.clusters.42paris.fr>
2023-06-26 16:54:02 +0200

    python ex04 looks ok

--- P00/ex04/operations.py
+++ P00/ex04/operations.py
@@ -1,3 +1,31 @@
 import sys
 
+if len(sys.argv) < 3:
+	print("Usage: python operations.py <number1> <number2>\nExample:\npython operations.py 10 3")
+	sys.exit(1)
+elif len(sys.argv) > 3:
+	assert False, "too many arguments"
+
+# print(sys.argv)
+
+try:
+	nb1 = int(sys.argv[1])
+	nb2 = int(sys.argv[2])
+	# print("nb1 ", nb1, " nb2 ", nb2)
+except Exception:
+	assert False, "argument is not an integer"
+
+def aff(nb1, nb2):
+	print("Sum:		", nb1 + nb2)
+	print("Difference:	", nb1 - nb2)
+	print("Product:	", nb1 * nb2)
+	if nb2 == 0:
+		print("Quotient: 	 ERROR (division by zero)")
+		print("Remainder: 	 ERROR (division by zero)")
+		sys.exit(1)
+	print("Quotient:	", nb1 / nb2)
+	print("Remainder:	", nb1 % nb2)
+
+aff(nb1, nb2)
+
 sys.exit(1)
