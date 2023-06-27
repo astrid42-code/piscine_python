870bbff4351ab8e77d2152dd042cd71792cf57fc
Astrid GAULTIER <asgaulti@bess-f1r5s3.clusters.42paris.fr>
2023-06-27 13:18:45 +0200

    python ex05 totalement fini

--- P00/ex05/kata04.py
+++ P00/ex05/kata04.py
@@ -4,11 +4,21 @@ import sys
 kata = (0, 4, 132.42222, 10000, 12345.67)
 zero = kata[0]
 four = kata[1]
-float_nb = kata[2]
-thousand = kata[3]
-other_float = kata[4]
+float_nb = '{:.2f}'.format(kata[2])
+thousand = '{:.2e}'.format(kata[3])
+other_float = '{:.2e}'.format(kata[4])
+sep = ""
 
-print("module_", zero, zero, ", ex_", zero, four, " : ")
+print(f"module_", zero, zero, ", ex_", zero, four, " : ", float_nb, ", ", thousand, ", ", other_float, sep="" )
 
 
 sys.exit(1)
+
+# resultat attendu :
+# $> python3 kata04.py
+# module_00, ex_04 : 132.42, 1.00e+04, 1.23e+04
+# $> python3 kata04.py | cut -c 10,18
+# ,:
+
+# doc :
+# https://python.sdv.univ-paris-diderot.fr/03_affichage/
