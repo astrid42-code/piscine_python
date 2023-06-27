f80d718c153212a03125fc9f6a914efbea269afa
Astrid GAULTIER <asgaulti@bess-f2r4s13.clusters.42paris.fr>
2023-06-27 17:29:38 +0200

    python ex06 creation dico

--- P00/ex06/recipe.py
+++ P00/ex06/recipe.py
@@ -1,4 +1,23 @@
 import sys
 
+# creation of a dictionnary {clé: valeur}
+cookbook = {
+    'sandwich': {'ingredients': 'ham, bread, cheese, tomatoes', 'meal': 'lunch', 'prep_time': 10},
+    'cake': {'ingredients': 'flour, sugar, eggs', 'meal': 'dessert', 'prep_time': 60},
+    'salad': {'ingredients': 'avocado, arugula, tomatoes, spinach', 'meal': 'lunch', 'prep_time': 15},
+}
+
+for keys in cookbook.items():
+    # for keys in sandwich.items():
+    print(keys)
+    # print(values)
 
 sys.exit(1)
+
+# cookbook contient des recettes avec :
+#     > name de la recipe qui est associe a : 
+#         > ingredients
+#         > meal (type de repas ex lunch)
+#         > prep_time (ne peut etre un nbr negatif!!!)
+
+# dans fct pour ajouter une recette avec input user penser a demander un nbr positif only (a check dans parsing)
