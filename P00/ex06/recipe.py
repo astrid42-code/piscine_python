5725f50516aa67ac12c8a39dd856d1855f249e91
Astrid GAULTIER <asgaulti@bess-f4r6s2.clusters.42paris.fr>
2023-06-28 16:26:48 +0200

    python ex06 looks ok

--- P00/ex06/recipe.py
+++ P00/ex06/recipe.py
@@ -2,15 +2,70 @@ import sys
 
 # creation of a dictionnary {clé: valeur}
 cookbook = {
-    'sandwich': {'ingredients': 'ham, bread, cheese, tomatoes', 'meal': 'lunch', 'prep_time': 10},
-    'cake': {'ingredients': 'flour, sugar, eggs', 'meal': 'dessert', 'prep_time': 60},
-    'salad': {'ingredients': 'avocado, arugula, tomatoes, spinach', 'meal': 'lunch', 'prep_time': 15},
+    "sandwich": {'ingredients': 'ham, bread, cheese, tomatoes', 'meal': 'lunch', 'prep_time': 10},
+    "cake": {'ingredients': 'flour, sugar, eggs', 'meal': 'dessert', 'prep_time': 60},
+    "salad": {'ingredients': 'avocado, arugula, tomatoes, spinach', 'meal': 'lunch', 'prep_time': 15},
 }
 
-for keys in cookbook.items():
-    # for keys in sandwich.items():
-    print(keys)
-    # print(values)
+def aff_names():
+    size = len(cookbook)
+    recipes = list(cookbook.keys())[:size]
+    print("List of recipes in our wonderful cookbook:", recipes)
+
+def print_recipe():
+    name = input("Please enter the name of the recipe you want:\n")
+    if name in cookbook:
+        print("You will need", cookbook[name]['ingredients'])
+        print("Eat it for", cookbook[name]['meal'])
+        print("It will take you", cookbook[name]['prep_time'], "minutes")
+    else:
+        print("There is no recipe with that name; you can add it with option 1")
+
+def delete_recipe():
+    name = input("Please enter the name of the recipe you want to delete:\n")
+    if name in cookbook:
+        del cookbook[name]
+    else:
+        print("There is no recipe with that name")
+
+def add_recipe():
+    name = input("Please enter a name for your recipe:\n")
+    ingredients = input("Please enter ingredients for your recipe:\n")
+    meal = input("Please enter the type of meal for your recipe:\n")
+    prep_time = input("Please enter preparation time for your recipe:\n")
+    cookbook[name] = {'ingredients': ingredients, 'meal': meal, 'prep_time': prep_time}
+    print("Congrats! You just add the", name, "recipe!")
+
+
+def exit_cookbook():
+    print("Cookbook closed. Goodbye !")
+    sys.exit(1)
+
+def main():
+    print("Welcome to the Python Cookbook !")
+    print("List of available option:")
+    print("	1: Add a recipe\n	2: Delete a recipe\n	3: Print a recipe\n	4: Print the cookbook\n	5: Quit")
+    while 1:
+        nb = input("Please select an option:\n")
+        if (nb == '1'):
+            add_recipe()
+        elif (nb == '2'):
+            delete_recipe()
+        elif (nb == '3'):
+            print_recipe()
+        elif (nb == '4'):
+            aff_names()
+
+            # print('Ingredients list:', cookbook[nb]['ingredients'])
+            
+        elif (nb == '5'):
+            exit_cookbook()
+        else:
+            print("This option doesn't exist")
+    
+if __name__ == "__main__":
+    main()
+
 
 sys.exit(1)
 
@@ -19,5 +74,3 @@ sys.exit(1)
 #         > ingredients
 #         > meal (type de repas ex lunch)
 #         > prep_time (ne peut etre un nbr negatif!!!)
-
-# dans fct pour ajouter une recette avec input user penser a demander un nbr positif only (a check dans parsing)
