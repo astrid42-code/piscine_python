1524629dd7db1320d03c8d3544cf5725df879a47
Astrid GAULTIER <asgaulti@bess-f4r6s2.clusters.42paris.fr>
2023-07-01 17:04:01 +0200

    ex10 en cours de chargement (hahaha...)

--- P00/ex10/loading.py
+++ P00/ex10/loading.py
@@ -1,4 +1,60 @@
 import sys
+import time
 
+def ft_progress(lst):
+    for i in lst:
+        print("i = ", i)
+        yield i
+
+def main():
+    # if len(sys.argv) == 1:
+    #     ac = input("Give me a number please :\n")
+    # elif (len(sys.argv) == 2):
+    #     ac = argv[1]
+    # else:
+    #     print("Wrong number of arguments, try again")
+    listy = range(10)
+    ret = 0
+    for elem in ft_progress(listy):
+        print("crotte")
+        ret += (elem + 3) % 5
+        time.sleep(0.01)
+    print()
+    print(ret)
+
+if __name__ == "__main__":
+    main()
 
 sys.exit(1)
+
+
+# import sys, time
+
+# def ft_progress(lst):
+#     start_time = time.time()
+#     for i in range(len(lst)):
+        
+#         sys.stdout.write('\r')
+#         sys.stdout.write("[%-20s] %d%%" % ('=' * int(20 * i / len(lst)), 1 * i / len(lst) * 100))
+#         sys.stdout.write(" [%d/%d]" % (i, len(lst)))
+#         sys.stdout.write(" elapsed time %d seconds" % (time.time() - start_time))
+#         sys.stdout.flush()
+#         yield i
+#     i += 1
+#     sys.stdout.write('\r')
+#     sys.stdout.write("[%-20s] %d%%" % ('=' * int(20 * i / len(lst)), 1 * i / len(lst) * 100))
+#     sys.stdout.write(" elapsed time %d seconds" % (time.time() - start_time))
+#     sys.stdout.write(" [%d/%d]" % (i, len(lst)))
+
+
+# def main():
+#     listy = range(500)
+#     ret = 0
+#     for elem in ft_progress(listy):
+#         ret += (elem + 3) % 5
+#         time.sleep(0.01)
+#     print()
+#     print(ret)
+
+# if __name__=="__main__":
+#     main()
